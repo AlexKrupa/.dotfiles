6c200643859cd882dc4c60dd6c1982b5adb7b572
Alex Krupa <alex.krupa@gmail.com>
2024-09-01 21:50:19 +0200

    Update .brewfile Update .config/fish/config.fish Update .gitconfig-base

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,3 +11,14 @@ starship init fish | source
 fish_vi_key_bindings # Vim mode, fish_default_key_bindings for default
 fzf_configure_bindings
 fx --comp fish | source
+
+# yy shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+# https://yazi-rs.github.io/docs/quick-start#shell-wrapper
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
