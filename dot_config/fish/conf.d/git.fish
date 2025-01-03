39de43293f9566b338fc8c91cab9ba2bcbe47aa9
Alex Krupa <alex.krupa@gmail.com>
2025-01-03 21:19:11 +0100

    Add .config/fish/conf.d/git.fish

---
+++ dot_config/fish/conf.d/git.fish
@@ -0,0 +1,42 @@
+function _git_main
+  for branch in "trunk" "main" "master"
+    if git rev-parse "$branch" &>/dev/null
+      echo $branch
+      break
+    end
+  end
+end
+
+function branch
+  if test (count $argv) -eq 0
+    git branch --sort=-committerdate
+  else
+    git checkout -b "$(whoami).$(string join '-' $argv | string replace -a ' ' '-').$(date +%Y-%m-%d)"
+  end
+end
+
+function gco --wraps="git checkout"
+  if test (count $argv) -gt 0
+    git checkout $argv
+  else
+    git checkout (_git_main)
+  end
+end
+
+function rebase
+  set trunk (_git_main)
+  git checkout $trunk
+  and git pull --prune
+  and git checkout -
+  and git rebase $trunk
+end
+
+function catch-up
+  git checkout (_git_main)
+  and git pull --prune
+end
+
+function rm-merged-local
+  set trunk (_git_main)
+  git branch --merged $trunk | command grep -v $trunk | xargs git branch -D
+end
