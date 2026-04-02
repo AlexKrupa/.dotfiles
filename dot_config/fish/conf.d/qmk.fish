ab02e16c58c40d2bba907e7953eab653c871f776
Alex Krupa <alex.krupa@gmail.com>
2026-04-02 21:57:45 +0200

    Add .config/fish/conf.d/qmk.fish

---
+++ dot_config/fish/conf.d/qmk.fish
@@ -0,0 +1,5 @@
+# QMK toolchain - keg-only Homebrew formulas need explicit PATH entries
+fish_add_path /opt/homebrew/opt/arm-none-eabi-gcc@8/bin
+fish_add_path /opt/homebrew/opt/arm-none-eabi-binutils/bin
+fish_add_path /opt/homebrew/opt/avr-gcc@8/bin
+fish_add_path /opt/homebrew/opt/avr-binutils/bin
