6fbd71185aafe266fce1e654b4b7339bfb2e03e5
Alex Krupa <alex.krupa@gmail.com>
2026-08-15 21:50:25 +0200

    Update .config/fish/conf.d/gradle.fish

--- dot_config/fish/conf.d/gradle.fish
+++ dot_config/fish/conf.d/gradle.fish
@@ -9,6 +9,18 @@ function gw --wraps=gradle --description 'Run Gradle via wrapper with profiling'
   end
 end
 
+function gwb --wraps=gradle --description 'Run Gradle via wrapper with build-brief'
+  set -l GW "$(upfind gradlew)"
+  if [ -z "$GW" ]
+    echo "Gradle wrapper not found."
+    return 1
+  else if contains -- "-p" $argv
+    build-brief $GW --parallel $argv
+  else
+    build-brief $GW -p $(dirname $GW) --parallel $argv
+  end
+end
+
 alias grp "$EDITOR $GRADLE_USER_HOME/gradle.properties"
 alias gri "$EDITOR $GRADLE_USER_HOME/init.gradle.kts"
 alias grccc "rm -rf $GRADLE_USER_HOME/configuration-cache"
