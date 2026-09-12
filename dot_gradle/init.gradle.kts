ccc62e01302025fd1131672dd17f7ec9093dac54
Alex Krupa <alex.krupa@gmail.com>
2026-09-12 11:29:15 +0200

    Update .gradle/init.gradle.kts

--- dot_gradle/init.gradle.kts
+++ dot_gradle/init.gradle.kts
@@ -1,41 +1,40 @@
 initscript {
-    repositories {
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.adarshr:gradle-test-logger-plugin:4.0.0")
-    }
+  repositories {
+    gradlePluginPortal()
+  }
+  dependencies {
+    classpath("com.adarshr:gradle-test-logger-plugin:4.0.0")
+  }
 }
 
 allprojects {
-    afterEvaluate {
-        if (extensions.findByName("testlogger") == null) {
-            apply<com.adarshr.gradle.testlogger.TestLoggerPlugin>()
-            extensions.configure<com.adarshr.gradle.testlogger.TestLoggerExtension> {
-                showExceptions = true
-                showStackTraces = true
-                showFullStackTraces = false
-                showCauses = true
-                slowThreshold = 1000
-                showSummary = true
-                showSimpleNames = true
-                showPassed = false
-                showSkipped = false
-                showFailed = true
-                // showOnlySlow = false
-                showStandardStreams = false
-                showPassedStandardStreams = true
-                showSkippedStandardStreams = true
-                showFailedStandardStreams = true
-                logLevel = LogLevel.LIFECYCLE
-            }
-        }
+  afterEvaluate {
+    if (extensions.findByName("testlogger") == null) {
+      apply<com.adarshr.gradle.testlogger.TestLoggerPlugin>()
+      extensions.configure<com.adarshr.gradle.testlogger.TestLoggerExtension> {
+        showExceptions = true
+        showStackTraces = true
+        showFullStackTraces = false
+        showCauses = true
+        slowThreshold = 1000
+        showSummary = true
+        showSimpleNames = true
+        showPassed = false
+        showSkipped = false
+        showFailed = true
+        // showOnlySlow = false
+        showStandardStreams = false
+        showPassedStandardStreams = true
+        showSkippedStandardStreams = true
+        showFailedStandardStreams = true
+        logLevel = LogLevel.LIFECYCLE
+      }
     }
+  }
 }
 
 gradle.afterProject {
-    tasks.withType<Test>().configureEach {
-        maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
-    }
+  tasks.withType<Test>().configureEach {
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+  }
 }
-
